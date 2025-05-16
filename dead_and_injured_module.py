80a1439075111cb7f493f4bf9d721c5017dc75cd
DonaldXoftDev <chibuezeokon1007@mail.com>
2025-05-15 22:15:22 -0700

    My first commit

---
+++ dead_and_injured_module.py
@@ -0,0 +1,105 @@
+import random
+import itertools
+
+new_possible_pins = [list(p) for p in itertools.permutations(range(10), 4)]
+
+def generate_user_pin():
+    u_pin = []
+    for i in range(4):
+        while True:
+            try:
+                user_choice = int(input(f'Enter the pin number{i + 1} from (0 - 9): '))
+                if user_choice not in u_pin and user_choice in range(0, 10):
+                    u_pin.append(user_choice)
+                    break
+                else:
+                    print(f'That number has either already been entered! or may not be within the range of 0 - 9!')
+            except ValueError:
+                print('That is not a number')
+    return u_pin
+
+
+def get_user_guesses():
+    u_guesses = []
+    for j in range(4):
+        guessing = True
+        while guessing:
+            try:
+                guess = int(input(f'Enter guess number {j + 1}: '))
+                if guess in range(10) and guess not in u_guesses:
+                    u_guesses.append(guess)
+                    guessing = False
+                else:
+                    print('That number has either already been guessed or is not within the range of 0 - 9!')
+            except ValueError:
+                print('That is not a number')
+    return u_guesses
+
+
+def generate_computer_pin():
+    comp_pin = random.choice(new_possible_pins)
+    return comp_pin
+
+
+def generate_computer_guesses():
+    comp_guesses = random.choice(new_possible_pins)
+    return comp_guesses
+
+
+# User tries to guess the computer pin with his 4 guesses
+# loop through all the user's guess and loop through the computer's pin
+# when a number in the user's guess is in the same position as a number in the computer's pin, it count's as a dead
+# when a number in the user's guess is in the computer's pin but at the correct index , it counts as an injured
+
+def compare_guesses(guesses, pin):
+    dead = 0
+    inj = 0
+
+    for i in range(len(guesses)):
+        for j in range(len(pin)):
+            if guesses[i] == pin[j] and i == j:
+                dead += 1
+            elif guesses[i] in pin:
+                index_for_injured = pin.index(guesses[i])
+                if i != index_for_injured:
+                    inj += 1
+                    break
+
+
+
+    return {"dead": dead, 'injured': inj}
+
+def message_feedback(game_feed_back, current_player, opponent):
+    if game_feed_back['dead'] == 4 and game_feed_back['injured'] == 0:
+        print(f'All dead')
+    else:
+        print(f"{current_player.title()}'s feedback from {opponent.title()}:{game_feed_back['dead']}dead and {game_feed_back['injured']}injured.")
+    print('\n')
+
+
+
+# computer's strategy for guessing the user's pin
+# the computer will generate all the possible combinations from a list of allowed guess range 0 - 9
+# the computer will check his guess against the user's pin and compare the feedback with all the
+# possible combinations in the list of possibilities to try and narrow down what the combinations
+# that will produce the same result
+# the computer will then move those combinations to a new list and then replace the old combination
+# -list with a copy of the new possibilities' list and clear the new list
+
+
+def narrow_down_guess(comp_curr_guess, user_pin):
+    feedback_to_computer = compare_guesses(comp_curr_guess, user_pin)
+    message_feedback(feedback_to_computer, 'computer', 'player')
+
+    global new_possible_pins
+    new_possibilities = []
+
+    for pin in new_possible_pins:
+        pin_feed_back = compare_guesses(comp_curr_guess, pin)
+        if pin_feed_back['dead'] == feedback_to_computer['dead'] and pin_feed_back["injured"] == feedback_to_computer['injured']:
+            new_possibilities.append(pin)
+
+    new_possible_pins = new_possibilities
+
+# When you generate the list of all possible combination, then you compare each combination with the computer's
+# current guess to try to reproduce the output it got as feedback from the user.
---
+++ main.py
@@ -0,0 +1,39 @@
+from dead_and_injured_module import (generate_user_pin,
+                                     generate_computer_pin, get_user_guesses, generate_computer_guesses,
+                                     compare_guesses, narrow_down_guess, message_feedback)
+
+user_pin = generate_user_pin()
+computer_pin = generate_computer_pin()
+
+
+
+print('\n')
+print(f'User pin : {user_pin}')
+print(f'Computer pin : {computer_pin}')
+
+Start_Game = True
+while Start_Game:
+    user_guesses = get_user_guesses()
+    computer_is_guessing_again = True
+    computer_guesses = generate_computer_guesses()
+
+    print(f'\nuser guessed : {user_guesses}')
+    print(f'computer guessed : {computer_guesses}')
+    print('\n')
+
+    # user tries to guess the computer's pin
+    feed_back_to_user = compare_guesses(user_guesses, computer_pin)
+    message_feedback(feed_back_to_user,"player", 'computer')
+
+    # computer tries to guess the player's pin as well with a random 4 digit guesses
+    # computer compares his current guess with all possible combinations of the guess to
+    # get the same feedback and narrow down what the user's pin might be
+    narrow_down_guess(computer_guesses, user_pin,)
+
+    # the second guess of the computer will be taken from one of the possibilities list
+
+
+
+
+
+
