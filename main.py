cdd9fd3834ccde35cdfacc2989b6bd41d5effabb
DonaldXoftDev <chibuezeokon1007@mail.com>
2025-05-17 01:47:26 -0700

    added a play again feature

--- main.py
+++ main.py
@@ -7,49 +7,53 @@ from arts import game_logo, welcome_image
 
 dead_and_injured_table = PrettyTable()
 dead_and_injured_table.field_names = ['User guesses', 'Feedback to user']
+def play_game():
+    game_icons = [welcome_image, game_logo]
+    print(' '.join(game_icons))
 
-game_icons = [welcome_image, game_logo]
-print(' '.join(game_icons))
+    user_pin = generate_user_pin()
+    computer_pin = generate_computer_pin()
 
-user_pin = generate_user_pin()
-computer_pin = generate_computer_pin()
+    winner = ''
 
-winner = ''
+    print(f'\n--------PLAYER PIN----------')
+    print(f'Your PIN: {user_pin}')
+    print(f'computer: {computer_pin}')
 
-print(f'\n--------PLAYER PIN----------')
-print(f'Your PIN: {user_pin}')
-print(f'computer: {computer_pin}')
+    Start_Game = True
+    while Start_Game:
+        user_guesses = get_user_guesses()
+        computer_guesses = generate_computer_guesses()
 
-Start_Game = True
-while Start_Game:
-    user_guesses = get_user_guesses()
-    computer_guesses = generate_computer_guesses()
+        print('\n')
+        print('-------------------GUESSES----------------------------')
+        print(f'user guessed : {user_guesses}')
+        print(f'computer guessed : {computer_guesses}\n')
 
-    print('\n')
-    print('-------------------GUESSES----------------------------')
-    print(f'user guessed : {user_guesses}')
-    print(f'computer guessed : {computer_guesses}\n')
+        print('-----------------------FEEDBACKS------------------------')
+        # user tries to guess the computer's pin
+        feed_back_to_user = compare_guesses(user_guesses, computer_pin)
+        message = message_feedback(feed_back_to_user, "player")
+        dead_and_injured_table.add_row([f'{user_guesses}', f'{message}'])
+        print(dead_and_injured_table)
 
-    print('-----------------------FEEDBACKS------------------------')
-    # user tries to guess the computer's pin
-    feed_back_to_user = compare_guesses(user_guesses, computer_pin)
-    message = message_feedback(feed_back_to_user, "player")
-    dead_and_injured_table.add_row([f'{user_guesses}', f'{message}'])
-    print(dead_and_injured_table)
+        if feed_back_to_user['dead'] == 4 and feed_back_to_user['injured'] == 0:
+            Start_Game = False
+            winner = 'player'
+        computer_bool = narrow_down_guess(computer_guesses, user_pin)
+        if computer_bool:
+              winner = 'computer'
+              Start_Game = False
+        # computer tries to guess the player's pin as well with a random 4 digit guesses
+        # computer compares his current guess with all possible combinations of the guess to
+        # get the same feedback and narrow down what the user's pin might be
 
-    if feed_back_to_user['dead'] == 4 and feed_back_to_user['injured'] == 0:
-        Start_Game = False
-        winner = 'player'
-    computer_bool = narrow_down_guess(computer_guesses, user_pin)
-    if computer_bool:
-          winner = 'computer'
-          Start_Game = False
-    # computer tries to guess the player's pin as well with a random 4 digit guesses
-    # computer compares his current guess with all possible combinations of the guess to
-    # get the same feedback and narrow down what the user's pin might be
+    print('\nTHE GAME HAS ENDED 🙂')
+    print('-----------------------RESULTS----------------------')
+    print(f'The winner is......{winner.title()}')
+    print(f'Your PIN : {user_pin}')
+    print(f'Computer PIN : {computer_pin}')
 
-print('\nTHE GAME HAS ENDED 🙂')
-print('-----------------------RESULTS----------------------')
-print(f'The winner is......{winner.title()}')
-print(f'Your PIN : {user_pin}')
-print(f'Computer PIN : {computer_pin}\nGoodbye 🤗')
+while input('Do you want to play a game of DEAD and INJURED? 😏: ').lower().strip().startswith('y'):
+    play_game()
+print('Goodbye 🤗')
